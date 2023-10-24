c3a6574b46e14b59eae6468b1d7fdb0b70d2b99a
Wolroys <alekseypaslenov@gmail.com>
2023-10-24 22:51:10 +0300

    added eureka server for application

---
+++ shop-server/src/main/java/com/wolroys/shopserver/ShopServerApplication.java
@@ -0,0 +1,15 @@
+package com.wolroys.shopserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ShopServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopServerApplication.class, args);
+    }
+
+}
